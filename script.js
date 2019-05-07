55d67a7f04f02ff261a6c5e1cac606bfd6b67ca2
Jörg Stöpel <stoepel@inloox.net>
2019-05-07 16:48:46 +0200

    new sidenav

--- script.js
+++ script.js
@@ -31,16 +31,16 @@ document.addEventListener('DOMContentLoaded', event => {
 /* Set the width of the side navigation to 250px and the left margin of the page content to 250px and add a black background color to body */
 function openNav() {
 	document.getElementById('mySidenav').style.width = '250px';
-	document.getElementById('main').style.marginLeft = '250px';
-	document.getElementById('main').style.opacity = '0.4';
+	// document.getElementById('main').style.marginLeft = '250px'; // deactivate if you want to use sidenav push w/opacity
+	// document.getElementById('main').style.opacity = '0.4'; // deactivate if you want to use sidenav push w/opacity
 	document.addEventListener('click', function closeNav() {});
 }
 
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
 function closeNav() {
 	document.getElementById('mySidenav').style.width = '0';
-	document.getElementById('main').style.marginLeft = '0';
-	document.getElementById('main').style.opacity = '1';
+	// document.getElementById('main').style.marginLeft = '0'; // deactivate if you want to use sidenav push w/opacity
+	// document.getElementById('main').style.opacity = '1'; // deactivate if you want to use sidenav push w/opacity
 }
 
 /* click in the sidenav will close it */
